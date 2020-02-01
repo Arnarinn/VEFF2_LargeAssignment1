5ce1b9efe8b18d377d7b08c33af2d70dd2681178
NjallMyrdal <33736143+NjallMyrdal@users.noreply.github.com>
2020-02-01 11:25:23 +0000

    njall push 1

--- makeBelieve.js
+++ makeBelieve.js
@@ -1,17 +1,19 @@
 
-var __ = (function(){
-	'use strict';
-	var Constructor = function(cssSelector){
-		console.log(cssSelector);
-		this.elems = document.querySelectorAll(cssSelector);
-	}
-	Constructor.prototype.parent = function(){
-		console.log("this is empty");
-	}
-	Constructor.prototype.parent = function(cssSelector){
-		this.elems.parentNode;
+
+(function (globalObj) {
+    "use strict"
+
+    function MakeBelieveElement(nodes) {
+
+        this.nodes = nodes;
 	}
 
+	/*
+	
 
-	return Constructor;
-})
+
+
+	*/
+
+    globalObj.__ = query;
+})(window);
